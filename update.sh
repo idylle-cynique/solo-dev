98d3561b1f394983815d424ff82e4b2fc1b50416
idylle-cynique <maltese_harvest@aol.com>
2026-01-02 00:42:57 +0900

    Revert "chore: 検証用"
    This reverts commit ea113732802cea9ecb649673b2fd81c8061a4619.

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@
 set -e
 
 TOOLS_DIR=".dev-tools"
-REPO="idylle-cynique/self-dev-facilitation#feature/20/kaizen-intro-and-update"
+REPO="idylle-cynique/self-dev-facilitation"
 
 echo "🔄 Self Dev Facilitation を更新しています..."
 
